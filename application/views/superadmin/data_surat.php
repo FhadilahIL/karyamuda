771e1bf121ff8358f60fc375e6ae210a6b53cfec
Muhammad Ilham Fhadilah <ilhammahier@gmail.com>
2021-08-26 06:31:54 +0700

    Update Surat View

--- application/views/superadmin/data_surat.php
+++ application/views/superadmin/data_surat.php
@@ -6,8 +6,29 @@
                 <div class="card-header ">
                     <h3>Data Surat Menyurat</h3>
                 </div>
-                <div class="card-body ">
-                    <p>Isi Data Pengguna</p>
+                <div class="card-body">
+                    <button class="btn btn-primary"><i class="fas fa-plus"></i> Tambah Data Surat</button>
+                    <table class="table table-responsive table-hover table-datatables">
+                        <thead class="text-center bg-warning text-light">
+                            <th>No.</th>
+                            <th style="min-width: 200px;">No. Surat</th>
+                            <th style="min-width: 300px;" class="text-left">Keterangan Surat</th>
+                            <th style="min-width: 180px;">Tanggal Surat</th>
+                            <th style="min-width: 120px;">Action</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-center">1.</td>
+                                <td class="text-center">001/KTKM/VIII/2021</td>
+                                <td class="text-justify">Surat Undangan Rapat</td>
+                                <td class="text-center">26 Agustus 2021</td>
+                                <td class="text-center">
+                                    <a href="#" class="text-waring"><i class="fas fa-edit"></i></a>
+                                    <a href="#" class="text-danger"><i class="fas fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
